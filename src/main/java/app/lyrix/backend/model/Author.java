406f1a463b8d7416c59f404c0033724bfed3806c
SohamNagi <nagisoham@gmail.com>
2024-10-17 04:11:14 -0400

    postgres working, crud api working and models working

--- src/main/java/app/lyrix/backend/model/Author.java
+++ src/main/java/app/lyrix/backend/model/Author.java
@@ -1,19 +1,53 @@
 package app.lyrix.backend.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
+import java.util.Objects;
 
-
+@Getter
 @Entity
+@NoArgsConstructor  // No-arg constructor required by JPA
+@AllArgsConstructor // Optional: Generates an all-args constructor
 public class Author {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int id;
+    private int id;
 
+    @Setter
     private String name;
 
-    // Optional: One-to-Many if you want a bidirectional relationship
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Song> songList;
+
+    // Optional: Additional custom logic (if needed)
+    public void addSong(Song song) {
+        song.setAuthor(this);
+        songList.add(song);
+    }
+
+    // Override toString for better logging and debugging
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    // Override equals and hashCode for entity comparison
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return id == author.id && Objects.equals(name, author.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
